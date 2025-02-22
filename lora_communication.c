dd972c7291937aa2c710e31091115603a01af137
JavaJumper <59884606+JumperOnJava@users.noreply.github.com>
2025-02-22 22:32:14 +0200

    http

--- lora_communication.c
+++ lora_communication.c
@@ -6,6 +6,18 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <pigpio.h>
+#define HTTPSERVER_IMPL
+#include "httpserverlib/httpserver.h"
+
+#define RESPONSE "Hello, World!"
+
+void handle_request(struct http_request_s* request) {
+  struct http_response_s* response = http_response_init();
+  http_response_status(response, 200);
+  http_response_header(response, "Content-Type", "text/plain");
+  http_response_body(response, RESPONSE, sizeof(RESPONSE) - 1);
+  http_respond(request, response);
+}
 
 // Callback function for transmission completion
 void tx_f(txData *tx)
@@ -45,6 +57,9 @@ int main()
     modem.eth.OCP = 240;              // Over-current protection (max current in mA)
     modem.eth.implicitHeader = 0;     // Use explicit header mode
     modem.eth.syncWord = 0x12;        // Set sync word
+
+    struct http_server_s* server = http_server_init(8008, handle_request);
+    http_server_listen(server);
 
     if (LoRa_begin(&modem) != 0)
     {
